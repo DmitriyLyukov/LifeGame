f44f31bb6fe20ac67e63131b29a05c9285eba9cc
Dmitry Lyukov <d.lyukov@ya.ru>
2016-02-11 23:12:47 +0300

    Refactoring

--- main.pas
+++ main.pas
@@ -8,7 +8,7 @@ uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Grids,
   ExtCtrls, StdCtrls;
 
-const m=40; n=40;
+const m = 40; n = 40;
 
 type
 
@@ -44,149 +44,159 @@ type
 
 var
   Form1: TForm1;
-  A:Field;
-  counter:integer;
+  A: Field;
+  counter: integer;
 implementation
 
 {$R *.lfm}
 
 { TForm1 }
 
-procedure readField(var t:text; var A:Field);
- var i,j:integer;
- begin
+procedure readField(var t: text; var A: Field);
+var i, j: integer;
+begin
   system.reset(t);
-  for i:=0 to m-1 do begin
-   for j:=0 to n-1 do read(t,A[i,j]);
-   if i<>m then readln(t);
+  for i := 0 to m - 1 do begin
+    for j := 0 to n - 1 do read(t, A[i, j]);
+    if (i <> m) then readln(t);
   end;
   system.close(t);
- end;
+end;
 
 
-function NeighborsInTor(var A:Field; i,j:integer):integer;
- var sum,x,y,l:integer;
- begin
-  sum:=0;
-  for l:=1 to 9 do begin
-   x:=i-1+(l-1) div 3;
-   y:=j-1+(l+2) mod 3;
-   if x=-1 then x:=m-1 else if x=m then x:=0;
-   if y=-1 then y:=n-1 else if y=n then y:=0;
-   if A[x,y]='#' then sum:=sum+1;
-                   end;
-  if A[i,j]='#' then sum:=sum-1;
-  NeighborsInTor:=sum;
- end;
+function NeighborsInTor(var A: Field; i, j: integer): integer;
+var sum, x, y, l: integer;
+begin
+  sum := 0;
+  for l := 1 to 9 do begin
+    x := i - 1 + (l - 1) div 3;
+    y := j - 1 + (l + 2) mod 3;
+    if (x = -1) then
+      x := m - 1
+    else if (x=m) then
+      x := 0;
+    if (y = -1) then
+      y := n - 1
+    else if (y = n) then
+      y := 0;
+    if (A[x, y] = '#') then
+      sum := sum + 1;
+  end;
+  if A[i, j] = '#' then
+    sum := sum - 1;
+  NeighborsInTor := sum;
+end;
 
-function neighbors(var A:Field; i,j:integer; InTor:boolean):integer;
- var sum,x,y,l:integer;
- begin
-  if  InTor
-    then neighbors:=NeighborsInTor(A,i,j)
-    else begin
-  sum:=0;
-  for l:=1 to 9 do begin
-   x:=i-1+(l-1) div 3;
-   y:=j-1+(l+2) mod 3;
-   if (x<>-1) and (x<>m) and (y<>-1) and (y<>n) and (A[x,y]='#')
-     then sum:=sum+1;
-                   end;
-  if A[i,j]='#' then sum:=sum-1;
-  neighbors:=sum;
+function neighbors(var A: Field; i, j: integer; InTor: boolean): integer;
+var sum, x, y, l: integer;
+begin
+  if  InTor then
+    neighbors := NeighborsInTor(A, i, j)
+  else begin
+    sum := 0;
+    for l := 1 to 9 do begin
+      x := i - 1 + (l - 1) div 3;
+      y := j - 1 + (l + 2) mod 3;
+      if (x <> -1) and (x <> m) and (y <> - 1) and (y <> n) and (A[x, y] = '#')
+      then sum := sum + 1;
     end;
- end;
+  if A[i, j] = '#' then
+    sum := sum - 1;
+  neighbors := sum;
+  end;
+end;
 
-procedure step(var A:Field; InTor:boolean);
- var B:Field;
-     i,j:integer;
- begin
-  B:=A;
-  for i:=0 to m-1 do
-   for j:=0 to n-1 do
-    if (A[i,j]='#') and not(neighbors(B,i,j,InTor) in [2,3]) then A[i,j]:='_'
-     else if (A[i,j]<>'#') and (neighbors(B,i,j,InTor)=3) then A[i,j]:='#';
- end;
+procedure step(var A: Field; InTor: boolean);
+var B: Field;
+    i, j: integer;
+begin
+  B := A;
+  for i := 0 to m - 1 do
+    for j := 0 to n - 1 do
+      if (A[i, j] = '#') and not(neighbors(B, i, j, InTor) in [2, 3]) then
+        A[i, j] := '_'
+      else if (A[i, j] <> '#') and (neighbors(B, i, j, InTor) = 3) then
+        A[i, j] := '#';
+end;
 
 
 procedure TForm1.FormCreate(Sender: TObject);
-var t:text;
+var t: text;
 begin
- system.assign(t,'input3.txt');
- readField(t,A);
- counter := 0;
-end;
-
-procedure TForm1.PathChange(Sender: TObject);
-begin
-
+  system.assign(t, 'input3.txt');
+  readField(t, A);
+  counter := 0;
 end;
 
 procedure TForm1.dgDrawCell(Sender: TObject; aCol, aRow: Integer;
   aRect: TRect; aState: TGridDrawState);
 begin
- if A[aRow,aCol]='#' then
-    dg.Canvas.Brush.Color:= clWhite
- else
-    dg.Canvas.Brush.Color:= clBlack;
+  if (A[aRow, aCol] = '#') then
+    dg.Canvas.Brush.Color := clWhite
+  else
+    dg.Canvas.Brush.Color := clBlack;
   dg.Canvas.Rectangle(aRect);
 end;
 
 procedure TForm1.dgSelectCell(Sender: TObject; aCol, aRow: Integer;
   var CanSelect: Boolean);
 begin
-  if A[aRow,aCol]='#' then A[aRow,aCol] := '_' else A[aRow,aCol] := '#';
+  if (A[aRow, aCol] = '#') then
+    A[aRow, aCol] := '_'
+  else
+    A[aRow, aCol] := '#';
 end;
 
 procedure TForm1.StartPauseClick(Sender: TObject);
 begin
   tmr.Enabled := not tmr.Enabled;
   if tmr.Enabled
-    then StartPause.Caption:='Pause'
-    else StartPause.Caption:='Start';
+    then StartPause.Caption := 'Pause'
+    else StartPause.Caption := 'Start';
 end;
 
 procedure TForm1.PathClick(Sender: TObject);
 begin
-  if SelectFile.Execute then Path.Text:=SelectFile.FileName;
+  if SelectFile.Execute then
+    Path.Text := SelectFile.FileName;
 end;
 
 procedure TForm1.NextGeneration;
 begin
- step(A,InTor.Checked);
- dg.Repaint;
- counter:=counter+1;
- Caption := 'Generation ' + IntToStr(counter);
+  step(A, InTor.Checked);
+  dg.Repaint;
+  counter := counter + 1; 
+  Caption := 'Generation ' + IntToStr(counter);
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
 begin
- NextGeneration;
+  NextGeneration;
 end;
 
 procedure TForm1.Button3Click(Sender: TObject);
-var i,j:integer;
+var i, j: integer;
 begin
- for i:=0 to m-1 do
-   for j:=0 to n-1 do A[i,j]:='_';
- dg.Repaint;
- counter:=0;
- Caption := 'Generation 0';
+  for i := 0 to m - 1 do
+    for j := 0 to n - 1 do
+      A[i, j] := '_';
+  dg.Repaint;
+  counter := 0;
+  Caption := 'Generation 0';
 end;
 
 procedure TForm1.LoadFileClick(Sender: TObject);
-var t:text;
+var t: text;
 begin
- system.assign(t,Path.Text);
- readField(t,A);
- counter := 0;
- dg.Repaint;
+  system.assign(t, Path.Text);
+  readField(t, A);
+  counter := 0;
+  dg.Repaint;
 end;
 
 procedure TForm1.tmrTimer(Sender: TObject);
 begin
- NextGeneration;
+  NextGeneration;
 end;
 
 end.
-
